e3c2cc397526355b60cdedad36dd1b97b4792873
Brent <puddletowndesign@gmail.com>
2017-04-01 15:36:06 -0600

    updated zsh preferences

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ cd ~/Documents/Dev
 git clone https://github.com/PuddletownDesign/ZSH
 
 ln -s ~/Documents/Dev/ZSH/puddletown.zsh-theme ~/.oh-my-zsh/themes/puddletown.zsh-theme
+ln -s ~/Documents/Dev/ZSH/.zshrc ~/.zshrc
 
 echo "set the theme in preferences in the lower right to load from a folder"
