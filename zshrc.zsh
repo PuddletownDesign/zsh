6480a2f6b3bbffe3d2d978d8f23ea66db9477e7f
Brent <puddletowndesign@gmail.com>
2020-11-08 03:11:28 -0800

    2020-11-08 back up

--- zshrc.zsh
+++ zshrc.zsh
@@ -111,8 +111,8 @@ plugins=(
   zsh-completions
   zsh-interactive-cd
   zsh-navigation-tools
-  zsh-history-substring-search
-  zsh-syntax-highlighting
+  # zsh-history-substring-search
+  # zsh-syntax-highlighting
   # thefuck
   # puddletown-docker
   # chucknorris
@@ -156,8 +156,8 @@ export SSH_KEY_PATH="~/.ssh/id_rsa"
 autoload -Uz compinit && compinit
 
 # Load shell integrations
-source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source $ZSH_CUSTOM/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+# source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+# source $ZSH_CUSTOM/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
 source $ZSH_CONFIG/iterm2_shell_integration.zsh
 source $ZSH/oh-my-zsh.sh
 
